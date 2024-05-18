38e16cacf4bde10a1083352ca48a183b3203514a
Rain_ThePrince <fedyayurchenko@gmail.com>
2024-05-18 18:14:22 +0500

    New files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,7 @@ window.onload = function() {
     document.getElementById("SneakerImage3").getElementsByTagName("img")[0].src = "assets/img/Sneakers3.png";
     document.getElementById("SneakerImage4").getElementsByTagName("img")[0].src = "assets/img/Sneakers4.png";
 };
+
+function openPersonalAccount() {
+    window.location.href = 'Account.html';
+}
