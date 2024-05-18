d1e536f9e256fccafd64dbddc2104afe4c67d9f1
Rain_ThePrince <fedyayurchenko@gmail.com>
2024-05-18 17:58:38 +0500

    JS added

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("SneakerImage1").getElementsByTagName("img")[0].src = "assets/img/Sneakers1.png";
+    document.getElementById("SneakerImage2").getElementsByTagName("img")[0].src = "assets/img/Sneakers2.png";
+    document.getElementById("SneakerImage3").getElementsByTagName("img")[0].src = "assets/img/Sneakers3.png";
+    document.getElementById("SneakerImage4").getElementsByTagName("img")[0].src = "assets/img/Sneakers4.png";
+};
